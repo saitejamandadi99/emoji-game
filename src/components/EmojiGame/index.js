cc80beff6c711fe33d5cb6554f30968d61e650ba
saitejamandadi99 <saitejamandadi99>
2024-10-03 09:18:18 +0000

    added navbar item

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -3,11 +3,41 @@ Quick Tip
 
 - Use the below function in the EmojiGame Component to shuffle the emojisList every time when an emoji is clicked.
 
-const shuffledEmojisList = () => {
-  const {emojisList} = this.props
-  return emojisList.sort(() => Math.random() - 0.5)
-}
+
 
 */
 
 // Write your code here.
+
+import {Component} from 'react'
+import NavBar from '../NavBar'
+import './index.css'
+
+class EmojiGame extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      emojisList: props.emojisList,
+      score: 0,
+      topScore: 0,
+    }
+  }
+
+  shuffledEmojisList = () => {
+    const {emojisList} = this.state
+    const shuffledList = emojisList.sort(() => Math.random() - 0.5)
+    this.setState({emojisList: shuffledList}) // Update state with shuffled list
+  }
+
+  render() {
+    const {emojisList, score, topScore} = this.state // Access emojisList from state
+
+    return (
+      <div className="appContainer">
+        <NavBar score={score} topScore={topScore} />
+      </div>
+    )
+  }
+}
+
+export default EmojiGame
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,1 +1,23 @@
 // Write your code here.
+import './index.css'
+
+const NavBar = props => {
+  const {score, topScore} = props
+  return (
+    <div className="headerContainer">
+      <div className="logoContainer">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+          alt="emoji logo"
+          className="emojilogo"
+        />
+        <h1 className="heading">Emoji Game</h1>
+      </div>
+      <div className="scoreContainer">
+        <p className="p">Score: {score}</p>
+        <p className="p">Top Score: {topScore}</p>
+      </div>
+    </div>
+  )
+}
+export default NavBar
